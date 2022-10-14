a586be41c66fd6afcab5ca1a8376e3d397b930f6
Selrahc <char.mailley@gmail.com>
2022-10-14 11:19:12 +0200

    1.5

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // seven wonders
+        // Debut du jeu
         Chess chess = new Chess();
         chess.play();
     }
